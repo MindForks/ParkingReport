9d925bf8a23a4a445b5b462e89286cfa1b1d9daa
TimurMirzaiev <timur.mirzaiev@gmail.com>
2019-06-22 15:32:37 +0300

    changes

---
+++ PR.Web/Controllers/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PR.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Edit()
+        {
+            //todo create views for methods by model
+            return View();
+        }
+        public IActionResult View()
+        {
+            return View();
+        }
+    }
+}
