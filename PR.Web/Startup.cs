9363acafc734251ace5abad3014f76523ee6012c
Михайло Саввін <savvin@dlit.dp.ua>
2019-06-22 20:00:52 +0300

    changes

--- PR.Web/Startup.cs
+++ PR.Web/Startup.cs
@@ -60,7 +60,7 @@ namespace PR.Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Reporter_Report}/{action=List}/{id?}");
             });
         }
     }
