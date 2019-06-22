076d404b8dad720e5a56ec5d8f0b9ffa634c70d8
Михайло Саввін <savvin@dlit.dp.ua>
2019-06-22 10:51:58 +0300

    created projects

---
+++ ParkingReport/PR.Business/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PR.Business
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
